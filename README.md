b3199fe3a6684dbef56f2c225a1f00327f68ec4a
ImDaBigBoss <67973871+ImDaBigBoss@users.noreply.github.com>
2022-01-02 13:05:12 +0100

    Fixed for latest Geyser version

--- README.md
+++ README.md
@@ -6,9 +6,10 @@
 PlaceholderAPI support for [Geyser](https://github.com/GeyserMC/Geyser)
 
 ## Placeholders
-* geyser_minecraft_version: e.g.: 1.16.5
+* geyser_java_version: e.g.: 1.16.5
 * geyser_bedrock_version: e.g.: 1.16.200
 * geyser_version: The current Geyser version
 * geyser_git_version: e.g.: git-master-fb18a64
 * geyser_platform: The player's platform e.g.: Win10, Switch, Android...
 * geyser_game_version: The player's game version
+* geyser_input_mode: The player's input mode
--- src/main/java/com/github/imdabigboss/geyserplaceholders/Placeholder.java
+++ src/main/java/com/github/imdabigboss/geyserplaceholders/Placeholder.java
@@ -1,11 +1,14 @@
 package com.github.imdabigboss.geyserplaceholders;
 
 import com.nukkitx.protocol.bedrock.BedrockPacketCodec;
+
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+
 import org.bukkit.entity.Player;
-import org.geysermc.connector.GeyserConnector;
-import org.geysermc.connector.network.BedrockProtocol;
-import org.geysermc.connector.network.session.GeyserSession;
+
+import org.geysermc.geyser.GeyserImpl;
+import org.geysermc.geyser.session.GeyserSession;
+import org.geysermc.geyser.network.MinecraftProtocol;
 
 import java.util.List;
 
@@ -49,34 +52,52 @@ public class Placeholder extends PlaceholderExpansion {
         }
 
         switch (identifier) {
-            case "minecraft_version":
-                return GeyserConnector.MINECRAFT_VERSION;
+            case "java_version":
+                return getJavaVersion();
             case "bedrock_version":
                 return getBedrockVersion();
             case "version":
-                return GeyserConnector.VERSION;
+                return GeyserImpl.VERSION;
             case "git_version":
-                return GeyserConnector.GIT_VERSION;
+                return GeyserImpl.GIT_VERSION;
             case "platform":
                 return getPlayerPlatform(player);
             case "game_version":
                 return getPlayerGameVersion(player);
+            case "input_mode":
+                return getPlayerInputMode(player);
         }
 
         return null;
     }
 
     /**
+     * Get the supported java version
+     * @return The supported java version
+     */
+    public String getJavaVersion() {
+        String javaVersions;
+        List<String> supportedJavaVersions = MinecraftProtocol.getJavaVersions();
+        if (supportedJavaVersions.size() > 1) {
+            javaVersions = supportedJavaVersions.get(0) + " - " + supportedJavaVersions.get(supportedJavaVersions.size() - 1);
+        } else {
+            javaVersions = supportedJavaVersions.get(0);
+        }
+
+        return javaVersions;
+    }
+    
+    /**
      * Get the supported bedrock version
      * @return The supported bedrock version
      */
     public String getBedrockVersion() {
         String bedrockVersions;
-        List<BedrockPacketCodec> supportedCodecs = BedrockProtocol.SUPPORTED_BEDROCK_CODECS;
+        List<BedrockPacketCodec> supportedCodecs = MinecraftProtocol.SUPPORTED_BEDROCK_CODECS;
         if (supportedCodecs.size() > 1) {
             bedrockVersions = supportedCodecs.get(0).getMinecraftVersion() + " - " + supportedCodecs.get(supportedCodecs.size() - 1).getMinecraftVersion();
         } else {
-            bedrockVersions = BedrockProtocol.DEFAULT_BEDROCK_CODEC.getMinecraftVersion();
+            bedrockVersions = MinecraftProtocol.SUPPORTED_BEDROCK_CODECS.get(0).getMinecraftVersion();
         }
 
         return bedrockVersions;
@@ -84,14 +105,14 @@ public class Placeholder extends PlaceholderExpansion {
 
     /**
      * Get the platform of the specified player
-     * @param player The player to get the platform for
+     * @param player The player
      * @return The player's platform version
      */
     public String getPlayerPlatform(Player player) {
-        GeyserSession geyserPlayer = GeyserConnector.getInstance().getPlayerByUuid(player.getUniqueId());
+        GeyserSession geyserPlayer = GeyserImpl.getInstance().connectionByUuid(player.getUniqueId());
 
         if (geyserPlayer != null) {
-            return geyserPlayer.getClientData().getDeviceOS().toString();
+            return geyserPlayer.getClientData().getDeviceOs().toString();
         } else {
             return "";
         }
@@ -99,14 +120,29 @@ public class Placeholder extends PlaceholderExpansion {
 
     /**
      * Get the game version of the specified player
-     * @param player The player to get the game version for
+     * @param player The player
      * @return The player's game version
      */
     public String getPlayerGameVersion(Player player) {
-        GeyserSession geyserPlayer = GeyserConnector.getInstance().getPlayerByUuid(player.getUniqueId());
+        GeyserSession geyserPlayer = GeyserImpl.getInstance().connectionByUuid(player.getUniqueId());
 
         if (geyserPlayer != null) {
             return geyserPlayer.getClientData().getGameVersion();
+        } else {
+            return "";
+        }
+    }
+
+    /**
+     * Get the input mode of the specified player
+     * @param player The player
+     * @return The player's input mode
+     */
+    public String getPlayerInputMode(Player player) {
+        GeyserSession geyserPlayer = GeyserImpl.getInstance().connectionByUuid(player.getUniqueId());
+
+        if (geyserPlayer != null) {
+            return geyserPlayer.getClientData().getCurrentInputMode().toString();
         } else {
             return "";
         }
